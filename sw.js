041feb2149a9210df56f3effacc1ff93580f6bd8
Ewaleifohemmanuel1-max <ewaleifohemmanuel1@gmail.com>
2026-07-22 23:17:00 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE = "flipx-tools-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./risk-reward-calculator.html",
+  "./candle-pattern-validator.html",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png",
+  "./rr-manifest.json",
+  "./candle-manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response && response.status === 200) {
+          const clone = response.clone();
+          caches.open(CACHE).then((cache) => cache.put(event.request, clone));
+        }
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request).then(
+          (cached) => cached || new Response("Offline", { status: 503, statusText: "Offline" })
+        )
+      )
+  );
+});
